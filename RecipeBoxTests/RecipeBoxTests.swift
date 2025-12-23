a51ea7bea4386b8ca55e2cee54fea06869b3b32a
aserhatsahin <aserhatsahin@gmail.com>
2025-12-23 15:44:38 +0300

    Initial Commit

---
+++ RecipeBoxTests/RecipeBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBoxTests.swift
+//  RecipeBoxTests
+//
+//  Created by Ahmet Serhat Sahin on 23.12.2025.
+//
+
+import Testing
+@testable import RecipeBox
+
+struct RecipeBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
