f63a13c306b07dcbb2dde96986c92bbc2efe45e8
aserhatsahin <aserhatsahin@gmail.com>
2025-12-25 17:04:24 +0300

    set up MVC folders and add mock recipe data/assets
    - Create MVC-style groups: Controller, Model, View, Service, DataSource
    - Add local mock data under data/ (recipes.json)
    - Add image placeholders under images/
    - Add scaffold files: RecipesDataSource, RecipeService, Recipe/RecipeResponse, FavoritesStore, CustomRecipeCell, VCs

---
+++ RecipeBox/DataSource/RecipesDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  RecipesDataSource.swift
+//  RecipeBox
+//
+//  Created by Ahmet Serhat Sahin on 25.12.2025.
+//
+
+import Foundation
---
+++ RecipeBox/Model/FavoritesStore.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesStore.swift
+//  RecipeBox
+//
+//  Created by Ahmet Serhat Sahin on 25.12.2025.
+//
+
+import Foundation
